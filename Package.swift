a6d185f2c35c723ba923e49130b6567aad38ec86
Akira <akira.matsuda@me.com>
2020-11-11 02:28:21 +0900

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,13 +18,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/realm/realm-cocoa.git", from: "10.1.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "EazyStore",
-            path: "EazyStore/Classes"
+            path: "EazyStore/Classes",
+            dependencies: [
+                "Realm",
+                "RealmSwift",
+            ]
         )
     ]
 )
