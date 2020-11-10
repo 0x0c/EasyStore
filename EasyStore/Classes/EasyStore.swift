85f28669a46ffce4ffa43762c23a7f622c3b851d
Akira <akira.matsuda@me.com>
2020-11-11 02:21:21 +0900

    format

--- EasyStore/Classes/EasyStore.swift
+++ EasyStore/Classes/EasyStore.swift
@@ -9,24 +9,22 @@
 import RealmSwift
 
 open class EasyStore<T: Object> {
-    public enum UpdateType : String {
-        case add = "add"
-        case update = "update"
-        case delete = "delete"
+    public enum UpdateType: String {
+        case add
+        case update
+        case delete
     }
-    
-    public init() {
-        
-    }
-    
+
+    public init() {}
+
     open func notificationName() -> String {
         return "EasyStoreUpdateNotification"
     }
-    
+
     open func updateTypeKey() -> String {
         return "updateType"
     }
-    
+
     open func transaction(_ handler: (_ realm: Realm) -> Void) throws {
         do {
             let realm = try Realm()
@@ -37,14 +35,14 @@ open class EasyStore<T: Object> {
             throw error
         }
     }
-    
+
     open func postNotification(withUpdateType type: UpdateType) {
-        NotificationCenter.default.post(Notification(name: Notification.Name(rawValue: self.notificationName()), object: nil, userInfo: [self.updateTypeKey() : type]))
+        NotificationCenter.default.post(Notification(name: Notification.Name(rawValue: notificationName()), object: nil, userInfo: [updateTypeKey(): type]))
     }
-    
+
     open func write(_ handler: @escaping (_ realm: Realm) -> Void) throws {
         do {
-            try transaction { (realm) in
+            try transaction { realm in
                 handler(realm)
                 postNotification(withUpdateType: .add)
             }
@@ -52,14 +50,14 @@ open class EasyStore<T: Object> {
             throw error
         }
     }
-    
+
     open func create(_ obj: T) throws {
-        try self.add(obj, update: false)
+        try add(obj, update: false)
     }
-    
+
     open func add(_ obj: T, update: Bool = true) throws {
         do {
-            try transaction { (realm) in
+            try transaction { realm in
                 realm.add(obj, update: update)
                 postNotification(withUpdateType: .add)
             }
@@ -67,10 +65,10 @@ open class EasyStore<T: Object> {
             throw error
         }
     }
-    
+
     open func update(_ handler: @escaping () -> Void, silentUpdate: Bool = false) throws {
         do {
-            try transaction { (realm) in
+            try transaction { _ in
                 handler()
                 if silentUpdate == false {
                     postNotification(withUpdateType: .update)
@@ -80,7 +78,7 @@ open class EasyStore<T: Object> {
             throw error
         }
     }
-    
+
     open func delete(_ obj: T) throws {
         let realm = try Realm()
         try realm.write {
@@ -88,16 +86,16 @@ open class EasyStore<T: Object> {
             postNotification(withUpdateType: .delete)
         }
     }
-    
+
     open func observe(_ handler: @escaping (UpdateType) -> Void) -> NSObjectProtocol {
-        return NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: self.notificationName()), object: nil, queue: nil) {
+        return NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: notificationName()), object: nil, queue: nil) {
             if let userInfo = $0.userInfo {
                 let type = userInfo[self.updateTypeKey()] as! UpdateType
                 handler(type)
             }
         }
     }
-    
+
     open func removeObserver(_ observer: NSObjectProtocol) {
         NotificationCenter.default.removeObserver(observer)
     }
